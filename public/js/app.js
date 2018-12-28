3daa60905f6fd7d258349c14e035528822204884
Alex Kharouk <akharoukpp@gmail.com>
2018-12-28 17:02:36 +0000

    ADD more to ProductList.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="ui unstackable items">
+        Hello World! I'm just a no-good component!
+      </div>
+    );
+  }
+}
