64128988e332da11ef054d31d80420401e7c2209
Alex Kharouk <akharoukpp@gmail.com>
2018-12-30 08:53:05 +0000

    FIX eslint file to be compatible with on-the-fly react app.

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,22 @@ class ProductList extends React.Component {
     );
   }
 }
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className="item">
+        <div className="image">
+          <img src="/images/products/image-aqua.png"></img>
+        </div>
+        <div className="middle aligned content">
+          <div className="description">
+            <a>Fort Night</a>
+          </div>
+        </div>
+      </div>
+    )
+
+// ReactDOM.render([WHAT], [WHERE])
+ReactDOM.render(<ProductList />, document.getElementById("content"));
+ReactDOM.render(<Product />, Document.getElementById("content"));
