e57ab82cd41c36436b7f2e56999c49ac5f2e003f
Alex Kharouk <akharoukpp@gmail.com>
2019-01-02 09:30:04 +0000

    ADD and finish Product Component.

--- public/js/app.js
+++ public/js/app.js
@@ -18,6 +18,11 @@ class Product extends React.Component {
         <div className="middle aligned content">
           <div className="description">
             <a>Fort Night</a>
+            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img className="ui avatar image" src="images/avatars/daniel.jpg" />
           </div>
         </div>
       </div>
